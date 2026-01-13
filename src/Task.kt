db94cec727bc15e47a740e0145c61f60e25efce5
Grisha <grisha.zhivot@bk.ru>
2026-01-13 21:30:38 +0300

    Demonstrate equals, copyandtoString for Task

--- src/Task.kt
+++ src/Task.kt
@@ -9,3 +9,17 @@ data class Task(
     val priority : Priority,
     var isCompleted : Boolean = false
 )
+fun main() {
+    val task1 = Task("A", "aaa", Priority.low)
+    val task2 = Task("A", "aaa", Priority.medium)
+    val task3 = Task("A", "aaa", Priority.high)
+
+    val task4 = task1.copy(title = "B")
+    val task5 = task1.copy(title = "B")
+    println(task4.toString())
+    println("${task2.hashCode()}")
+    println("${task4.hashCode()}")
+    println("${task5.hashCode()}")
+
+    println(task4.equals(task5))
+}
