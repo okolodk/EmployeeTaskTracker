e4253e41729fbcaaee4a5a1670ff66c0b6ca653a
Grisha <grisha.zhivot@bk.ru>
2026-01-13 22:34:33 +0300

    Implement assignTaskmethodwith validation

--- src/Employee.kt
+++ src/Employee.kt
@@ -1,30 +1,52 @@
-//class Employee (
-//
-//) {
-//    private val fullName: String = "Алексеев Григорий Сергеевич"
-//    private val position : String = "Студент"
-//    var salary : Int = 60
-//        set(value) {
-//            if (value < 0) {
-//                println("Нельзя ставить значение мменьше 0")
-//                field = 0
-//            }
-//        }
-//    var yearsOfExperience : Int = 30
-//        set(years) {
-//            field = years.coerceIn(0,50)
-//        }
+class Employee (
+    fullName : String,
+    position : String,
+    salary : Int,
+    yearsOfExperience : Int
+) : ReportGenerator{
+    private val fullName: String = fullName
+    private val position : String = position
+    var salary : Int = salary
+        set(value) {
+            if (value < 0) {
+                println("Нельзя ставить значение мменьше 0")
+                field = 0
+            }
+        }
+    var yearsOfExperience : Int = yearsOfExperience
+        set(years) {
+            field = years.coerceIn(0,50)
+        }
 //    fun getfullName() :String = fullName
 //    fun getPosition() : String = position
 //    fun getSalary() : Int = salary
 //    fun getYearsOfExperience() : Int = yearsOfExperience
+    override fun generateReport(): String {
+        var str : String = "$fullName, $position, $salary, $yearsOfExperience"
+        return str
+    }
+    var currentTask: Task? = null
+    var isCompleted : Boolean = true
 
+    fun assignTask(newTask: Task) {
+        if (isCompleted) {
+            currentTask = newTask
+            isCompleted = false
+            println("Получил задачу")
+        }
+        else {
+            println("Сотрудник уже занят задачей ${currentTask?.title}")
+        }
+    }
 
-//}
-//fun main() {
-//    val employee = Employee()
+}
+fun main() {
+    val employee = Employee("aaa", "bbb", 30000, 30)
 //    employee.salary = -10
 //    employee.yearsOfExperience = 60
 //    println("${employee.salary}")
 //    println("${employee.yearsOfExperience}")
-//}
+    employee.assignTask(Task("A", "aaa", Priority.low))
+    employee.assignTask(Task("И", "aaa", Priority.low))
+
+}
--- src/ReportGenerator.kt
+++ src/ReportGenerator.kt
@@ -1,21 +1,16 @@
 interface ReportGenerator {
     fun generateReport(): String
 }
-class Employee(val name : String, val position : String) : ReportGenerator {
-    override fun generateReport(): String {
-        var str : String = "$name, $position и т.д."
-        return  str
-    }
-}
 
-fun main() {
-    val reportGenerators : List<ReportGenerator> = listOf(
-        Employee("AAA", "aaa"),
-        DevelopmentDepartment("BBB"),
-        Employee("CCC", "ccc"),
-        DevelopmentDepartment("DDD")
-    )
-    for (c in reportGenerators) {
-        println(c.generateReport())
-    }
-}
+
+//fun main() {
+//    val reportGenerators : List<ReportGenerator> = listOf(
+//        Employee("aaa", "bbb", 30000, 30),
+//        DevelopmentDepartment("BBB"),
+//        Employee("AAA", "BBB", 30000, 30),
+//        DevelopmentDepartment("DDD")
+//    )
+//    for (c in reportGenerators) {
+//        println(c.generateReport())
+//    }
+//}
